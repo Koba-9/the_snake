9715743df1736bacc0f7de89d204bfb5cb4fced6
Matvey Kobiakov <mat9kk@gmail.com>
2026-01-16 21:46:24 +0500

    1

--- the_snake.py
+++ the_snake.py
@@ -1,8 +1,7 @@
-import pygame
-
 from random import randint
 from typing import List, Tuple, Optional
 
+import pygame
 
 # Константы для размеров поля и сетки:
 SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
